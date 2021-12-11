168ced26ed4e88477b5ad8ccbb181939f10b633a
Evgzek <zekamusijhcuk@gmail.com>
2021-12-11 16:50:30 +1000

    Обработка ошибки и выбор действия

--- src/Interest.java
+++ src/Interest.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
 public class Interest {
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        System.out.println("Выберите операцию: 1)Вычислить финальное значение, 2)Вычислить процент. Если хотите выйти нажмите любую кнопку");
+        int destine;
+        try {
+            destine = p.nextInt();
+        }catch (Exception e){
+            System.out.println("Ошибка");
+            return;
+        }
+
+        }
+
+    }
 }
